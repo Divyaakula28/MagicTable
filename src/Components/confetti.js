257853e37de3aefd4028b8d6bc3dc784b2572faf
Divyaakula28 <divya2000akula@gmail.com>
2022-12-30 16:24:40 +0530

    chnage

--- src/Components/confetti.js
+++ src/Components/confetti.js
@@ -3,7 +3,10 @@ import Confetti from 'react-confetti'
 import '../CSS/confetti.css';
 
 export default () => {
+  const width= window.innerWidth;
+  const height=window.innerHeight;
   return (
-    <Confetti id='hideMeAfter5Seconds' numberOfPieces='700'/>
+    <Confetti id='hideMeAfter5Seconds' width={width}
+    height={height} numberOfPieces='700'/>
   )
 }
